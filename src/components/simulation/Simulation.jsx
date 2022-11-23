5dae7fdd9f8496163b6334f1aaa42b5fa6d12a58
Forza5 <ganp433@gmail.com>
2022-11-23 23:08:13 +0900

    모의면접 녹화 기능 typescript 추가

--- src/components/simulation/Simulation.jsx
+++ src/components/simulation/Simulation.jsx
@@ -61,35 +61,16 @@ const Simulation = () => {
         </FlexCol>
       </FlexCol>
       <FlexCol>
-        <div id="container">
-          <video id="gum" playsInline autoPlay muted></video>
-          <video id="recorded" playsInline loop></video>
-          <div>
-            <button id="start">Start camera</button>
-            <button id="record" disabled>
-              Start Recording
-            </button>
-            <button id="play" disabled>
-              Play
-            </button>
-            <button id="download" disabled>
-              Download
-            </button>
-          </div>
-          <div>
-            Recording format: <select id="codecPreferences" disabled></select>
-          </div>
-          <div>
-            <h4>Media Stream Constraints options</h4>
-            <p>
-              Echo cancellation: <input type="checkbox" id="echoCancellation" />
-            </p>
-          </div>
-
-          <div>
-            <span id="errorMsg"></span>
-          </div>
+        <div>
+          <canvas id="local_video_area">
+            <video id="local_video"></video>
+          </canvas>
         </div>
+        <button type="button">시작</button>
+        <button type="button" id="record_button">
+          녹화
+        </button>
+        <a id="download_link">다운로드</a>
       </FlexCol>
     </>
   );

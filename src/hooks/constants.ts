f9a1127873ecc810a6cf9a70aefaa4d39814de38
jaeyeoneej <5ikve@naver.com>
2022-12-01 22:42:07 +0900

    test: #14 스톱워치 기능 구현 22-12-01 20:42(정재연)

---
+++ src/hooks/constants.ts
@@ -0,0 +1,6 @@
+// 밀리세컨 단위로 렌더링 조정
+export const INTERVAL = 1000 as const;
+
+export const MILLISEC_PER_SECOND = 1000 as const;
+export const SECOND_PER_HOUR = 3600 as const;
+export const SECOND_PER_MINUTE = 60 as const;
